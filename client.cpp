330c9381f471d9b1e700d7fd2571cf1907ebafaa
xorud1541 <xorud1544@naver.com>
2019-01-11 21:38:22 +0900

    cpp file

--- client.cpp
+++ client.cpp
@@ -4,17 +4,19 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-
+#include <string>
 #define BUF_SIZE 100
 #define NAME_SIZE 20
-
+using namespace std;
 void * send_msg(void* arg);
 void * recv_msg(void* arg);
 void error_handling(char *arg);
 
-char nick[NAME_SIZE] = "[DEFAULT]";
+//char nick[NAME_SIZE] = "[DEFAULT]";
 char msg[BUF_SIZE];
-
+string nick;
+//string msg;
+int interface(int);
 int main(int argc, char* argv[]){
 	int sock;
 	struct sockaddr_in serv_addr;
@@ -36,79 +38,18 @@ int main(int argc, char* argv[]){
 		error_handling("connect() error");
 
 	while(1){
-
-		/* interface */
-		int num = 0;
-		int flag = -1;
-		printf("1. [join] \n");
-		printf("2. [log in] \n");
-		printf("3. [quit] \n");
-		scanf("%d", &num);
-		getchar();
-		if(num == 1){
-			char ack[4];
-			char msg[120];
-			char name[20];
-			char pwd[20];
-			char temp[20];
-			fflush(stdin);
-			fputs("ID >> ", stdout);
-			scanf("%s", name);
-			
-			fputs("PWD >> ", stdout);
-			scanf("%s", pwd);
-			
-			sprintf(msg, "1 %s %s", name, pwd);
-			strcpy(temp, name);
-		        write(sock, msg, strlen(msg));
-
-			int len = 0;
-			while(1) {
-				len = read(sock, ack, 4);
-				if(len != -1){
-				     ack[len] = 0;
-				     break;
-				}
-			}
-
-			if(!strcmp(ack, "yes")){
-				flag = 1;
-				printf("\n\njoin success!!\n");
-				printf("you can use a service right now!\n");
-				strcpy(nick, temp);
-			}
-
-			else{
-				printf("join fail!!\n");
-			}
-
-			if(flag > 0) break;
-			else
-				continue;
-
-
+		int s = interface(sock);
+		if(s==1){
+			cout << "success" << endl;
+			break;
 		}
-		else if(num == 2){
-			/* log in
-			 * if log in success, change flag = 1;
-			 *
-			 *
-			 * end */
-
-
-		}
-		else {
-
-			/* num == 3
-			 * quit
-			 *
-			 * close(sock);
-			 * return 0; */
-
+		else if(s==-1)
+			continue;
+		else
+		{
+			return 0;
 		}
 	}
-
-	printf("***************CHAT SERVICE******************\n");
 
 	pthread_create(&snd_thread, NULL, send_msg, (void*)&sock);
 	pthread_create(&rcv_thread, NULL, recv_msg, (void*)&sock);
@@ -118,18 +59,63 @@ int main(int argc, char* argv[]){
 	return 0;
 }
 
+int interface(int sock){
+	int num;
+	printf("1. [join]\n");
+	printf("2. [login]\n");
+	printf("3. [quit]\n");
+	cin >> num;
+
+
+	if(num == 1){
+		string id;
+		string pwd;
+		printf(" ID >> ");
+		cin >> id;
+		printf(" PWD >> ");
+		cin >> pwd;
+
+		string str = "1";
+		str = str + ' ' + id + ' ' + pwd;
+
+		/* send id and password i would like */
+
+		write(sock, str.c_str(), str.size());
+		
+		/* check message from server */
+		
+		char ack[4];
+		string temp;
+		int len = read(sock, ack ,4);
+		if(len != -1){
+			 ack[len] = 0;
+			 temp = string(ack);
+		}
+
+
+		if(temp.compare("yes") == 0){
+			nick = id;
+			return 1;
+		}
+		else
+			return -1;
+					
+	}
+	else if(num == 2){
+	}
+	else if(num==3){
+		return 0;
+	}
+}
+
 void * send_msg(void * arg){
 	int sock = *((int*)arg);
 	char name_msg[NAME_SIZE+BUF_SIZE+2];
-
 	while(1){
-		fgets(msg, BUF_SIZE, stdin);
-		if(!strcmp(msg, "q\n") || !strcmp(msg, "Q\n")){
-			close(sock);
-			exit(0);
-		}
-		sprintf(name_msg, "[%s]: %s", nick, msg);
-		write(sock, name_msg, strlen(name_msg));
+		fgets(msg, sizeof(msg), stdin);
+	
+		sprintf(name_msg, "[%s]: %s", nick.c_str(), msg);
+		write(sock, name_msg, sizeof(name_msg)-1);
 	}
 	return NULL;
 }
